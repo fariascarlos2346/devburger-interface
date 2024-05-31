15896c168466a07196f8257734583c1481a25d77
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-05-31 11:00:00 -0300

    estrutura HTML

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -1,9 +1,32 @@
-
+import { Container } from './styles';
+import Logo from '../../assets/logo.png';
 
 export function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt="logo dev"/>
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Olá, seja bem vindo a pagina do <span>desenvolvedor carlos!</span> 
+                    Acesse com seu<span>Login e senha.</span> 
+                </Title>
+                <form>
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type="email"/>
+                    </InputContainer>
+
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type="password"/>
+                    </InputContainer>
+                    <Link>Esqueci minha senha</Link>
+                    <button>Entrar</button>
+                </form>
+                <Link>Não possui conta? Clique aqui.</Link>
+            </RightContainer>
+        </Container>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Login } from './containers/login'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Login } from './containers/login';
+import GlobalStyles from './styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login/>
+    <GlobalStyles/>
   </React.StrictMode>,
-)
+);
