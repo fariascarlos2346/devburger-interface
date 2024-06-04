a5c1f0b2e371f41ddfdb6c28032d510ad94a696d
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-06-04 09:27:20 -0300

    criando a page register

---
+++ src/containers/Register/index.jsx
@@ -0,0 +1,89 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import * as yup from 'yup';
+
+import Logo from '../../assets/logo.png';
+import { Button } from '../../components/Button';
+import { api } from '../../services/api';
+import {
+    Container,
+    Form,
+    InputContainer,
+    LeftContainer,
+    RightContainer,
+    Title
+} from './styles';
+
+export function Register() {
+    const schema = yup
+        .object({
+            email: yup
+                .string()
+                .email('Digite um e-mail válido')
+                .required('O e-mail é obrigatório'),
+            password: yup
+                .string()
+                .min(6, 'A senha deve ter pelo menos 6 caracteres')
+                .required('Digite uma senha'),
+        })
+        .required();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    console.log(errors);
+
+    const onSubmit = async (data) => {
+        const response = await toast.promise(
+            api.post('/sessions', {
+                email: data.email,
+                password: data.password,
+            }),
+            {
+                pending: 'Verificando seus dados',
+                success: 'Seja Bem-vindo(a)',
+                error: 'Email ou Senha Incorretos',
+            }
+        );
+        
+        console.log(response);
+    };
+
+    return (
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt="logo dev" />
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Olá, seja bem vindo a pagina do <span>desenvolvedor Carlos!</span>
+                    <br />Acesse com seu <span> Login e senha.</span>
+                </Title>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type="email" {...register('email')} />
+                        <p>{errors?.email?.message}</p>
+                    </InputContainer>
+
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type="password" {...register('password')} />
+                        <p>{errors?.password?.message}</p>
+                    </InputContainer>
+                    <Button type="submit">Entrar</Button>
+                </Form>
+
+                <p>
+                    Não possui conta? <a>Clique aqui.</a>
+                </p>
+            </RightContainer>
+        </Container>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
-import { Login } from './containers/login';
 import GlobalStyles from './styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login/>
+    <RouterProvider router={router}/>
     <GlobalStyles/>
     <ToastContainer autoClose={2000} theme="dark"/>
   </React.StrictMode>,
